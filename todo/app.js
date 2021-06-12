1bb7a438fef2aea8295bf76542f0aaf6d79ced71
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-06-13 01:25:07 +0530

    to-do with work

--- todo/app.js
+++ todo/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
-var item = []
+let taskList = []
+let workList = [];
+var newItem = "";
 app.get("/", function (req, res) {
+    console.log(taskList,workList);
     var today = new Date();
     var currentDay = today.getDay();
     var day = "";
@@ -41,17 +46,39 @@ app.get("/", function (req, res) {
     //         day = "Saturday";
     //         break;
     // }
-    res.render("list", { day: day, item: item });
+    res.render("list", {
+        listTitle: day,
+        List: taskList
+    });
 });
 
 
 
 app.post("/", function (req, res) {
-    var newitem = req.body.newItem;
-    item.push(newitem);
-    res.redirect("/");
+    if (req.body.button === "Work") {
+        newItem = req.body.newItem;
+        workList.push(newItem);
+        res.redirect("/work")
+    } else {
+
+        newItem = req.body.newItem;
+        taskList.push(newItem);
+        res.redirect("/");
+    }
 });
 
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work",
+        List: workList
+    });
+});
+
+app.post("/work", function (req, res) {
+    newItem = req.body.newItem;
+    workList.push(newItem);
+    res.redirect("/work");
+});
 
 app.listen(3000, function () {
     console.log("server running on port 3000.")
