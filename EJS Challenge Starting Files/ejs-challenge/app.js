9d832a2bfee61f3d46b4ae02b2fde5e8a850ffdf
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-06-17 13:56:30 +0530

    ejs challenge 9 to 16

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -15,9 +15,10 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+let posts=[];
 
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent});
+  res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
 });
 
 
@@ -37,11 +38,27 @@ app.get("/compose",function(req,res){
 
 
 app.post("/compose",function(req,res){
-  console.log(req.body.composeContent);
+  
+  let postTitle=req.body.composeTitle;
+  let postContent=req.body.composeContent;
+  posts.push({
+    title:postTitle,
+    content:postContent
+  });
+  console.log(posts);
+  res.redirect("/");
 });
 
 
-
+app.get('/posts/:day', function (req, res) {
+  let requestedPost=res.send(req.params.postName);
+  posts.forEach(function(post){
+    if(post===requestedPost)
+    {
+      console.log(post);
+    }
+  })
+});
 
 
 
