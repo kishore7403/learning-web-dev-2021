ae28a0fd6b5c7b51a58b1ff0a7bb5c58ad90936f
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-06-17 22:32:49 +0530

    ejs challenge 9 to 21 completed

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -3,10 +3,13 @@
 const express = require("express");
 
 const app = express();
+const _ = require("lodash");
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 
 
@@ -15,47 +18,53 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts=[];
+let posts = [];
 
-app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
+app.get("/", function (req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
 
 
-app.get("/about",function(req,res){
-  res.render("about",{aboutContent:aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
-app.get("/contact",function(req,res){
-  res.render("contact",{contactContent:contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
 
-app.get("/compose",function(req,res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
 
-app.post("/compose",function(req,res){
-  
-  let postTitle=req.body.composeTitle;
-  let postContent=req.body.composeContent;
+app.post("/compose", function (req, res) {
+
+  let postTitle = req.body.composeTitle;
+  let postContent = req.body.composeContent;
   posts.push({
-    title:postTitle,
-    content:postContent
+    title: postTitle,
+    content: postContent
   });
-  console.log(posts);
   res.redirect("/");
 });
 
 
-app.get('/posts/:day', function (req, res) {
-  let requestedPost=res.send(req.params.postName);
-  posts.forEach(function(post){
-    if(post===requestedPost)
-    {
-      console.log(post);
+app.get('/posts/:postName', function (req, res) {
+  let requestedPost = _.lowerCase(req.params.postName);
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedPost) {
+      res.render("post",{title:post.title,content:post.content});
     }
   })
 });
@@ -63,6 +72,6 @@ app.get('/posts/:day', function (req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
