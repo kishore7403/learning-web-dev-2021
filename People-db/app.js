ad2f0ed778f0e32e6d19c40590b2905e06795c3b
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-06-25 19:11:32 +0530

    Establishing Relationships and Embedding

--- People-db/app.js
+++ People-db/app.js
@@ -2,31 +2,74 @@ const mongoose =require('mongoose');
 mongoose.connect("mongodb://localhost:27017/peopleDB",{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => console.log( 'Database Connected' ))
 .catch(err => console.log( err ));;
 
+
+const fruitSchema =new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true,"no name found"]
+    }
+});
 const peopleSchema =new mongoose.Schema({
     _id:Number,
     name:{
         type: String,
         required:[true,"no name found"]
     },
-    age: Number
+    age: Number,
+    favfruit: fruitSchema
 });
+
+
 
 const People = mongoose.model("People",peopleSchema);
+const Fruit=mongoose.model("Fruit",fruitSchema)
 
-const people =new People({
-    _id:6,
-    age:256
+const mango=new Fruit({
+    name:"mango"
+    
 });
 
+// const people =new People({
+//     _id:8,
+//     name:"felix",
+//     age:256,
+//     favfruit:peach
+// });
 
-people.save();
-People.find(function(err,people){
+
+mango.save();
+// people.save();
+
+
+
+
+
+// people.save();
+// People.find(function(err,people){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         people.forEach(function(item){
+//             console.log(item.name);
+//         });
+//     }
+// });
+
+// People.deleteOne({_id:4},function(err){
+//     if(err){
+//          console.log(err);
+//          }
+//     else{
+//         console.log("update sucessfull");
+//     }
+// });
+
+People.updateOne({_id:1},{favfruit:mango},function(err){
     if(err){
         console.log(err);
     }
     else{
-        people.forEach(function(item){
-            console.log(item.name);
-        });
+        console.log("update sucessfull");
     }
-});
+})
