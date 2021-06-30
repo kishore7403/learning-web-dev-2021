6b3dada22ae17dce608c0702beeaa373ea9d26aa
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-07-01 02:32:10 +0530

    added gitignore and procfile

--- todo DB/app.js
+++ todo DB/app.js
@@ -156,7 +156,13 @@ app.get("/:customListName",function(req,res){
     });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
 
-app.listen(3000, function () {
-    console.log("server running on port 3000.")
+}
+
+
+app.listen(port, function () {
+    console.log("server running")
 });
