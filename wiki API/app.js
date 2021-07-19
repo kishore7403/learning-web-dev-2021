08ca812bc4c292f77c99487d491e0c4736e8a4c2
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-07-19 23:22:26 +0530

    rest PUT

--- wiki API/app.js
+++ wiki API/app.js
@@ -1,55 +1,74 @@
-const express =require("express");
-const mongoose=require("mongoose");
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 
-mongoose.connect("mongodb+srv://kishore:1234@cluster0.q4gs6.mongodb.net/wikiDB",{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => console.log( 'Database Connected' ))
-.catch(err => console.log( err ));
+mongoose.connect("mongodb+srv://kishore:1234@cluster0.q4gs6.mongodb.net/wikiDB", {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('Database Connected'))
+    .catch(err => console.log(err));
 
-const articleSchema={
-    title:String,
-    content:String
+const articleSchema = {
+    title: String,
+    content: String
 };
 
-const Article=mongoose.model("Article",articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
+////////////////////////////////////////request targetting all articles///////////////
 
-app.get("/articles",function(req,res){
-    Article.find(function(err,foundArticles){
-        if(!err){
-            console.log(foundArticles);
-            res.send(foundArticles);
-        }
-        else{
-            res.send(err);
-        }
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                console.log(foundArticles);
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        newArticle.save();
+    })
+    .delete(function () {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("deleted all articles")
+            } else {
+                res.send(err);
+            }
+        })
+
     });
-});
 
-app.post("/articles",function(req,res){
+    ///////////////////////////////////////request targetting specicic articles///////////////
+app.route("/articles/:articleTitle")
 
-    const newArticle=new Article({
-        title:req.body.title,
-        content:req.body.content
-    })
-    newArticle.save();
-});
-
-app.delete("/articles",function(){
-    Article.deleteMany(function(err){
+.get(function(req,res){
+    Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
         if(!err){
-            res.send("deleted all articles")
+           res.send(foundArticle); 
         }
         else{
-            res.send(err);
+            res.send("no articles found");
         }
     })
-
 })
-app.listen(3000,function(){
+
+app.listen(3000, function () {
     console.log("server is up and running");
 })
